25215aeab3cbe9476aac1d8a3b39523af6b02d8b
IP8 <mx_2013@outlook.com>
2021-12-07 23:26:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/api_restful/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.api_restful;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
